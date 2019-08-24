6f75e624d3074f3ab35a102924817a097a29068c
조법룡 <jby0711823@naver.com>
2019-08-24 15:17:08 +0900

    bug fix

--- paintjs/app.js
+++ paintjs/app.js
@@ -31,6 +31,7 @@ function startPainting() {
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    console.log("now moving...");
     if (!painting) {
         ctx.beginPath();
         ctx.moveTo(x, y);
